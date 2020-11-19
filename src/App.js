9c56552e75bfa2806650c1e68996b152dd58563d
Dakshita Jena <djena1399@gmail.com>
2020-11-19 20:41:35 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import Aboutus from "./Aboutus/AboutUs";
+
+function App() {
+  return (
+    <div className="App">
+      <Aboutus/>
+    </div>
+  );
+}
+
+export default App;
